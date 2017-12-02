1cad112857f9d4439db0ffd660afb586739b17c8
Michael Easter <codetojoy@gmail.com>
2017-12-02 15:23:51 -0400

    simplify

--- egg_22_JLink_Cross_Platform/compile.sh
+++ egg_22_JLink_Cross_Platform/compile.sh
@@ -6,10 +6,8 @@ javac -d build/modules \
 `find src -name "*.java"`
 
 jar --create --file=lib/net.codetojoy.db@1.0.jar \
---module-version=1.0 -C build/modules/net.codetojoy.db . 
+-C build/modules/net.codetojoy.db . 
 
 jar --create --file=lib/net.codetojoy.service@1.0.jar \
---module-version=1.0 \
---main-class=net.codetojoy.service.impl.UserService \
 -C build/modules/net.codetojoy.service . 
 
